dc7af2e6d78b8e9d4de4cedafbba1d8acb92ba13
Bowen <mr_lbw@outlook.com>
2018-10-25 08:26:58 +0800

    feat(src/*): add two player roles

--- src/App/Square.jsx
+++ src/App/Square.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className='square'
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    )
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className='square'
+//         onClick={() => this.props.onClick()}
+//       >
+//         {this.props.value}
+//       </button>
+//     )
+//   }
+// }
+
+function Square (props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 export default Square
