2c4c138ac8089d7ccab5cc3d76141137a13245cd
Bowen <mr_lbw@outlook.com>
2018-10-28 13:09:38 +0800

    feat(App): complete time travel  functionality

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -9,14 +9,29 @@ class Game extends Component {
       history: [{
         squares: Array(9).fill(null)
       }],
+      stepNumber: 0,
       xIsNext: true
     }
   }
 
   render() {
     const history = this.state.history
-    const current = history[history.length - 1]
+    const current = history[this.state.stepNumber]
     const winner = calculateWinner(current.squares)
+
+    const moves = history.map((step, move) => {
+      const desc = move
+        ? `Go to move # ${move}`
+        : `Go to game start`
+
+      return (
+        <li key={move}>
+          <button
+            onClick={() => this.jumpTo(move)}
+          >{desc}</button>
+        </li>
+      )
+    })
 
     const status = winner
       ? `Winner: ${winner}`
@@ -39,7 +54,7 @@ class Game extends Component {
   }
 
   handleClick(i) {
-    const history = this.state.history
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)
     const current = history[history.length - 1]
     const squares = current.squares.slice()
     if (calculateWinner(squares) || squares[i]) return
@@ -49,7 +64,15 @@ class Game extends Component {
       history: history.concat([{
           squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
+    })
+  }
+
+  jumpTo (step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0
     })
   }
 }
