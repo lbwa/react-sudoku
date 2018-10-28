787403bdc977496002564802060e738f2a4d6312
Bowen <mr_lbw@outlook.com>
2018-10-28 20:15:33 +0800

    chore: publish docs

---
+++ docs/precache-manifest.7e0a7a14b71f4d33b07474136c8d4462.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "20247a84677d6ce7dcf2",
+    "url": "./static/js/main.20247a84.chunk.js"
+  },
+  {
+    "revision": "36afbc2ff7a701f5e8ee",
+    "url": "./static/js/1.36afbc2f.chunk.js"
+  },
+  {
+    "revision": "20247a84677d6ce7dcf2",
+    "url": "./static/css/main.8048e87d.chunk.css"
+  },
+  {
+    "revision": "919a41ee5d5b4b19c929463cf2733bf5",
+    "url": "./index.html"
+  }
+];
